eec4a92a87493e7fc287131f9cd1cc4df2813504
abdul <up2dul@gmail.com>
2024-06-06 01:25:19 +0700

    feat(routes): login page UI

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,11 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div>
-        <Link to="/">
-          Home
-        </Link>{' '}
-        <Link to="/login">
-          Login
-        </Link>
-      </div>
-      <hr />
+    <main>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </main>
   ),
 });
