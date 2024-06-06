d261d58a77d1eae6d0b2b87785fc29369cc45088
abdul <up2dul@gmail.com>
2024-06-06 17:19:03 +0700

    feat(pages): temporary settings content page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,3 @@
-import { useEffect } from 'react';
-import { useAuth } from '~/hooks/useAuth';
-
 export default function Settings() {
-  const { isAuthenticated } = useAuth();
-
-  useEffect(() => {
-    console.log(isAuthenticated);
-  }, [isAuthenticated]);
   return <div className="mt-32 text-2xl">Settings</div>;
 }
